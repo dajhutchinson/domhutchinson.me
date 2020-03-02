803e5ae6fde22ff164a52774d39b1d64585e37d5
dahutchinson <dom@domhutchinson.me>
2020-03-02 00:41:37 +0000

    Home page

---
+++ js/sidebar.js
@@ -0,0 +1,15 @@
+function about() {
+    document.getElementById("body").innerHTML="<h1>ABOUT</h1>";
+}
+
+function qualifications() {
+    document.getElementById("body").innerHTML="<h1>QUALIFICATIONS</h1>";
+}
+
+function experience() {
+    document.getElementById("body").innerHTML="<h1>EXPERIENCE</h1>";
+}
+
+function projects() {
+    document.getElementById("body").innerHTML="<h1>PROJECTS</h1>";
+}
