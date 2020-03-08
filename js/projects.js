45450a2ede4a973b4836171fb6833fa580f00c72
dahutchinson <dom@domhutchinson.me>
2020-03-08 01:18:34 +0000

    Experience writtern. Projects built. Qualifications done with flex.

---
+++ js/projects.js
@@ -0,0 +1,19 @@
+function project1() {
+    document.getElementById("projects_content").style.background="red";
+}
+
+function project2() {
+    document.getElementById("projects_content").style.background="blue";
+}
+
+function project3() {
+    document.getElementById("projects_content").style.background="orange";
+}
+
+function project4() {
+    document.getElementById("projects_content").style.background="pink";
+}
+
+function project5() {
+    document.getElementById("projects_content").style.background="green";
+}
