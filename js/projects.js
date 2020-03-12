92d2d77a5199a66642a434d9a45b238258a6d3c7
dahutchinson <dom@domhutchinson.me>
2020-03-12 00:47:32 +0000

    ONS Project

--- js/projects.js
+++ js/projects.js
@@ -1,19 +1,24 @@
-function project1() {
-    document.getElementById("projects_content").style.background="red";
+function ons() {
+    document.getElementById("projects_content").innerHTML='<div class=\"row\" style=\"height: 100%; padding-bottom: 20px\; background-color:#171717"> <div class=\"col-sm-9\" style=\"height: 100%\"> <div class=\"enivronment_header\"> <p class=\"left_page_subheading\">Effect of the Depreciation of Pound Sterling on UK Tourism.</p> <hr style=\"margin: 0 20px 10px 0; border-top-color: lightgray\"> </div> <div class=\"about_row\"> <div class=\"experience_box\" style=\"flex:1; margin-left: 0px\"> <p>This is a project that I worked on as part of the Office of National Statistics\' (ONS) \'University Working Group 2019-20\'</p> <p>I worked with 4 other undergraduates at my university (each studying different degrees) to analyse what effect the depreciations of Pound Sterling after the Great Recession in 2008 &amp; after the EU-Referendum in 2016 had on UK Tourism. The end requirements of this project was to produce a document summarising our findings &amp; a 40 minute presenation on our findings (See sidebar)</p> <hr style=\"margin: 0; border-top-color: lightgray\"> <p><b>Findings</b></p> <p style=\"margin: 0\">My focus during the project was on the accomodation industry. My key findings were:</p> <ul class=\"qual_overview_detail_ul\"> <li>The depreciations in 2008 &amp; 2016 had opposite effects on demand.</li> <li>Post-2008 demand contracted due to the effects of the recession being felt in much of the western world. Business travel was affected less than leisure travel. This is seen in the disparity between bedroom occupancy &amp; bedspace occupancy rates diverging.</li> <li>Post-2008 the number of short-stay accomodation establishments spiked due to people choosing to rent our second homes (or spare bedrooms) as new sources of income.</li> <li>Post-2016 demand increased due to the UK become a cheaper place for foreign nationals to come to (due to a favourable exchange rate) and foreign countries becoming more expensive for UK citizens to visit.</li> <li>Northern Ireland recived a significant boost in occupancy rates after the 2016 referendum, due to its accessibility to those in the Republic of Ireland &amp; its new relative cheapness.</li> </ul> <hr style=\"margin: 0; border-top-color: lightgray\"> <b>Takeaways</b> <ul class=\"qual_overview_detail_ul\"> <li>Focus on writing in a short, precise fashion more often. Rather than my usual, more verbose, style.</li> <li>Allow people to interpt overall trends from the graphs &amp; focus my writing on specific points.</li> <li>Smart choices during graph design can really help get a message accross.</li> <li>When working in a team it is much more important to have a unified message, than a unified style.</li> </ul> </div> </div> </div> <div class=\"col-sm-3\" style=\"height: 100%; border-left: 1px solid lightgray\"> <ul class=\"qual_overview_detail_ul\"> <li><a class=\"emp_anchor\">Year - </a>2019-20</li> <li><a class=\"emp_anchor\">Timescale - </a>3 Months</li> <li><a class=\"emp_anchor\">End User - </a> Office for National Statistics</li> <li><a class=\"emp_anchor\">Key Skills - </a> Data sourcing, high-level data analysis &amp; presenting.</li> </ul> <hr style=\"margin: 0 5%; border-top-color: lightgray\"> <p class=\"centred_page_sub_subheading\">Links</p> <hr style=\"margin: 0 5%; border-top-color: lightgray\"> <ul class=\"qual_overview_detail_ul\"> <li> <a class=\"emp_link_anchor\" href=\"docs/DepreciationOfThePoundOntravelAndTourism-Report.pdf\" target=\"_blank\">Report</a><br> My contributions can be found on pgs 8-10. </li> <li> <a class=\"emp_link_anchor\" href=\"docs/DepreciationOfThePoundOntravelAndTourism-PP.pdf\" target=\"_blank\">Presentation</a><br> My contributions can be found on slides 27-33. </li> </ul> </div> </div>';
+    document.getElementById("projects_content").style.background="";
 }
 
 function project2() {
     document.getElementById("projects_content").style.background="blue";
+    document.getElementById("projects_content").innerHTML="";
 }
 
 function project3() {
     document.getElementById("projects_content").style.background="orange";
+    document.getElementById("projects_content").innerHTML="";
 }
 
 function project4() {
     document.getElementById("projects_content").style.background="pink";
+    document.getElementById("projects_content").innerHTML="";
 }
 
 function project5() {
     document.getElementById("projects_content").style.background="green";
+    document.getElementById("projects_content").innerHTML="";
 }
