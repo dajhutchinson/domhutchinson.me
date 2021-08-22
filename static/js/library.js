2de605db591337f95f1e0aedc1cae844d13f9bc3
DH <domahutch@gmail.com>
2021-08-22 17:26:35 +0100

    Change font added global variables

--- static/js/library.js
+++ static/js/library.js
@@ -1,7 +1,5 @@
 function library_show_file(file) {
   var parent=document.getElementById("pdf_output");
   var html="<embed src='/static/docs/library/"+file+"' type='application/pdf' width='100%'' height='100%'>";
-  console.log("HELLO WORLD");
-  console.log(html);
   parent.innerHTML=html;
 }
